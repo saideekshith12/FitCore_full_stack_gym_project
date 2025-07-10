c37b5836c2cbe7189bd598d181fca0d64510ff47
sai deekshith <saideekshith06@gmail.com>
2025-07-10 13:55:52 +0530

    monodb

--- backend/db.js
+++ backend/db.js
@@ -3,14 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = async ()=>{
-    try {
-        mongoose.connect(process.env.MONGODB_URL)
-        console.log("Database is connected");
-    } catch (error) {
-        console.log("error in database connection", error)
-        
-    }
-}
+const db = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+    });
+    console.log("Database is connected");
+  } catch (error) {
+    console.error("Error in database connection:", error);
+  }
+};
 
-export default db
+export default db;
